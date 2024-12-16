a9c59a27f4e768454c423afa6e2222f81575012a
AliDeWeb <alimoradi0business@gmail.com>
2024-12-16 14:08:59 +0330

    initialize db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule } from './modules/logger/logger.module';
+import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './modules/db/db.module';
 
 @Module({
-  imports: [LoggerModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    LoggerModule,
+    DbModule,
+  ],
 })
 export class AppModule {}
---
+++ src/modules/db/db.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getEnv } from 'src/utils/getEnvs.util';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: getEnv('DB_URL'),
+      }),
+    }),
+  ],
+})
+export class DbModule {}
