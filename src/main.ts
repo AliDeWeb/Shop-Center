2bec6cc189f4fa3b4bdbf0c20ec6ba1a785dcdb7
AliDeWeb <alimoradi0business@gmail.com>
2024-12-16 19:41:18 +0330

    install and initialize swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { getEnv } from './utils/getEnv/getEnvs.util';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Shop-Center APIs Documentation')
+    .setDescription(
+      'Shop-Center is a backend service developed by AliDeWeb using NestJs.',
+    )
+    .setVersion('1.0.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('document', app, documentFactory);
 
   await app.listen(getEnv('PORT'));
 }
