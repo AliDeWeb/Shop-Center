285aea0b5f3eb015f804df405e2dea3e90bf77b6
AliDeWeb <alimoradi0business@gmail.com>
2024-12-17 17:06:33 +0330

    create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { LoggerModule } from './modules/logger/logger.module';
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './modules/db/db.module';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UserModule } from './modules/user/user.module';
     LoggerModule,
     DbModule,
     UserModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
