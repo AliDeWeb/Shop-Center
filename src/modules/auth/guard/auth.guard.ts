583eb2e572f6a3ed933484bf3b6b408549b0e050
AliDeWeb <alimoradi0business@gmail.com>
2024-12-19 01:06:12 +0330

    develop AuthGuard

---
+++ src/modules/auth/guard/auth.guard.ts
@@ -0,0 +1,49 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../../user/user.service';
+import mongoose from 'mongoose';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+  ) {}
+
+  async canActivate(context: ExecutionContext) {
+    const request: Request = context.switchToHttp().getRequest();
+
+    const cookies = request.cookies;
+
+    if (!cookies.refreshToken || !cookies.accessToken)
+      throw new ForbiddenException('login or register to continue!');
+
+    let payload: {
+      iat: number;
+      exp: number;
+      id: mongoose.Schema.Types.ObjectId;
+      username: string;
+    };
+    try {
+      payload = await this.jwtService.verifyAsync(cookies.accessToken);
+    } catch (error) {
+      throw new ForbiddenException('login or register to continue!');
+    }
+
+    const user = await this.userService.getUserById(payload.id);
+
+    if (payload.exp <= Math.floor(Date.now() / 1000))
+      throw new ForbiddenException('login or register to continue!');
+
+    // @ts-ignore
+    request.user = user;
+
+    return true;
+  }
+}
