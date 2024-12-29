fffcab31fe671108e1959ac3e7c5d6b99139729f
AliDeWeb <alimoradi0business@gmail.com>
2024-12-29 20:40:23 +0330

    develop create product in product service

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { ProductRepository } from './repo/product.repository';
 import { Schema } from 'mongoose';
 import { IProductDocument } from '../../types/product/product.interface';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
 export class ProductService {
@@ -13,5 +14,9 @@ export class ProductService {
     if (!result) throw new NotFoundException('Product not found');
 
     return result;
+  }
+
+  async createProduct(body: CreateProductDto): Promise<IProductDocument> {
+    return await this.productRepository.create(body);
   }
 }
