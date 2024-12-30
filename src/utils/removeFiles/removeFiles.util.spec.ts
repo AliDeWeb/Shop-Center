0b0ecaf1e13a5e53f57c013049a7af249fd69e07
AliDeWeb <alimoradi0business@gmail.com>
2024-12-30 19:19:55 +0330

    write test for removeFiles.util.ts

---
+++ src/utils/removeFiles/removeFiles.util.spec.ts
@@ -0,0 +1,23 @@
+import { removeFiles } from './removeFiles.util';
+import { promises as fs } from 'fs';
+import { NotFoundException } from '@nestjs/common';
+
+describe('removeFiles', () => {
+  it('should remove files if they exist', async () => {
+    jest.spyOn(fs, 'unlink').mockResolvedValue(undefined);
+
+    const result = await removeFiles(['files path']);
+
+    expect(result).toBeUndefined();
+    expect(fs.unlink).toHaveBeenCalledTimes(1);
+    expect(fs.unlink).toHaveBeenCalledWith('files path');
+  });
+
+  it('should throw err if files dont exist', async () => {
+    jest.spyOn(fs, 'unlink').mockRejectedValue(new Error('File not found'));
+
+    expect(removeFiles(['nonexistentFile.txt'])).rejects.toThrowError(
+      NotFoundException,
+    );
+  });
+});
